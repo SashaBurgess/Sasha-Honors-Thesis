7d7b2ce5f8e731347048ae22d830710e8edbcf97
SashaBurgess <alexandra_burgess@brown.edu>
2021-12-08 11:34:57 -0500

    Update FlyTissue_Diff_expressed.R

--- Final-Scripts/FlyTissue_Diff_expressed.R
+++ Final-Scripts/FlyTissue_Diff_expressed.R
@@ -7,11 +7,27 @@ cut=countdata[1,]>0
 groups=groups[cut,]
 
 ##EdgeR comparisons
-x2 <- countdata[,cut]
+x <- countdata[,cut]
 group <- factor(groups$Group)
-y2 <- DGEList(counts=x2,group=group)
-keep <- filterByExpr(y2)
-y2 <- y2[keep,,keep.lib.sizes=FALSE] 
-z2 <- calcNormFactors(y2, method = "TMM") 
-mz=z2
-cpmdata=cpm(z2,base=2)
+y <- DGEList(counts=x,group=group)
+keep <- filterByExpr(y)
+y <- y[keep,,keep.lib.sizes=FALSE] 
+z <- calcNormFactors(y, method = "TMM") 
+mz=z
+cpmdata=cpm(z,base=2)
+
+design<-model.matrix(~0+group)
+colnames(design) <- levels(group)
+z = estimateGLMCommonDisp(z,design, verbose=T)
+z = estimateGLMTrendedDisp(z,design)
+z = estimateGLMTagwiseDisp(z,design)
+fit <- glmFit(z, design)
+
+compare = makeContrasts(SOMCH-OOMCH, levels=design)
+lrt <- glmLRT(fit,contrast=as.vector(compare))		
+G_X_E<-topTags(lrt,adjust.method="BH",n = nrow(z$counts), sort.by="PValue")
+MCH=G_X_E$table
+sMCH=MCH[MCH$FDR<.05,]
+dim(sMCH)
+
+write.csv(sMCH, "/Users/Sasha.L.Burgess/Documents/GitHub/Sasha-Honors-Thesis/Final-tables/sig_SOMCHvsOOMCH.csv")
