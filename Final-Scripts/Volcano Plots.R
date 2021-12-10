4ac5f91a3b7da40ab191a6711aca8cfcf99779ed
SashaBurgess <alexandra_burgess@brown.edu>
2021-12-10 11:27:59 -0500

    Create Volcano Plots.R

---
+++ Final-Scripts/Volcano Plots.R
@@ -0,0 +1,18 @@
+countdata<-read.csv("https://raw.githubusercontent.com/SashaBurgess/Sasha-Honors-Thesis/main/Final-tables/countdata.csv", row.names = 1)
+groups<-read.csv("https://raw.githubusercontent.com/SashaBurgess/Sasha-Honors-Thesis/main/Final-tables/groups.csv", row.names = 1)
+cpmdata<-read.csv("https://raw.githubusercontent.com/SashaBurgess/Sasha-Honors-Thesis/main/Final-tables/cpmdata.csv", row.names = 1)
+
+sig_SOMCHvsOOMCH<-read.csv("https://raw.githubusercontent.com/SashaBurgess/Sasha-Honors-Thesis/main/Final-tables/sig_SOMCHvsOOMCH.csv", row.names = 1)
+sig_SOFCHvsOOFCH<-read.csv("https://raw.githubusercontent.com/SashaBurgess/Sasha-Honors-Thesis/main/Final-tables/sig_SOFCHvsOOFCH.csv", row.names = 1)
+sig_SOMCTvsOOMCT<-read.csv("https://raw.githubusercontent.com/SashaBurgess/Sasha-Honors-Thesis/main/Final-tables/sig_SOMCTvsOOMCT.csv", row.names = 1)
+sig_SOFCTvsOOFCT<-read.csv("https://raw.githubusercontent.com/SashaBurgess/Sasha-Honors-Thesis/main/Final-tables/sig_SOFCTvsOOFCT.csv", row.names = 1)
+sig_SOMCAvsOOMCA<-read.csv("https://raw.githubusercontent.com/SashaBurgess/Sasha-Honors-Thesis/main/Final-tables/sig_SOMCAvsOOMCA.csv", row.names = 1)
+sig_SOFCAvsOOFCA<-read.csv("https://raw.githubusercontent.com/SashaBurgess/Sasha-Honors-Thesis/main/Final-tables/sig_SOFCAvsOOFCA.csv", row.names = 1)
+
+SOMCHvsOOMCH<-read.csv("https://raw.githubusercontent.com/SashaBurgess/Sasha-Honors-Thesis/main/Final-tables/SOMCHvsOOMCH.csv", row.names = 1)
+SOFCHvsOOFCH<-read.csv("https://raw.githubusercontent.com/SashaBurgess/Sasha-Honors-Thesis/main/Final-tables/SOFCHvsOOFCH.csv", row.names = 1)
+SOMCTvsOOMCT<-read.csv("https://raw.githubusercontent.com/SashaBurgess/Sasha-Honors-Thesis/main/Final-tables/SOMCTvsOOMCT.csv", row.names = 1)
+SOFCTvsOOFCT<-read.csv("https://raw.githubusercontent.com/SashaBurgess/Sasha-Honors-Thesis/main/Final-tables/SOFCTvsOOFCT.csv", row.names = 1)
+SOMCAvsOOMCA<-read.csv("https://raw.githubusercontent.com/SashaBurgess/Sasha-Honors-Thesis/main/Final-tables/SOMCAvsOOMCA.csv", row.names = 1)
+SOFCAvsOOFCA<-read.csv("https://raw.githubusercontent.com/SashaBurgess/Sasha-Honors-Thesis/main/Final-tables/SOFCAvsOOFCA.csv", row.names = 1)
+
