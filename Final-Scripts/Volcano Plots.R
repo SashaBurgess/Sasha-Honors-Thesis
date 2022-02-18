64eca7b0c9d01edb10d9c99df70e19b1cb294e09
SashaBurgess <alexandra_burgess@brown.edu>
2022-02-18 12:13:49 -0500

    Update Volcano Plots.R

--- Final-Scripts/Volcano Plots.R
+++ Final-Scripts/Volcano Plots.R
@@ -1,18 +1,58 @@
+library(edgeR)
+library(plot3D)
+library("DESeq2")
+library(heatmaply)
+library(gplots)
+library(tidyverse)
+library(annotate)
+library(EnhancedVolcano)
+library(org.Mm.eg.db)
+
+
 countdata<-read.csv("https://raw.githubusercontent.com/SashaBurgess/Sasha-Honors-Thesis/main/Final-tables/countdata.csv", row.names = 1)
 groups<-read.csv("https://raw.githubusercontent.com/SashaBurgess/Sasha-Honors-Thesis/main/Final-tables/groups.csv", row.names = 1)
 cpmdata<-read.csv("https://raw.githubusercontent.com/SashaBurgess/Sasha-Honors-Thesis/main/Final-tables/cpmdata.csv", row.names = 1)
 
-sig_SOMCHvsOOMCH<-read.csv("https://raw.githubusercontent.com/SashaBurgess/Sasha-Honors-Thesis/main/Final-tables/sig_SOMCHvsOOMCH.csv", row.names = 1)
-sig_SOFCHvsOOFCH<-read.csv("https://raw.githubusercontent.com/SashaBurgess/Sasha-Honors-Thesis/main/Final-tables/sig_SOFCHvsOOFCH.csv", row.names = 1)
+
 sig_SOMCTvsOOMCT<-read.csv("https://raw.githubusercontent.com/SashaBurgess/Sasha-Honors-Thesis/main/Final-tables/sig_SOMCTvsOOMCT.csv", row.names = 1)
 sig_SOFCTvsOOFCT<-read.csv("https://raw.githubusercontent.com/SashaBurgess/Sasha-Honors-Thesis/main/Final-tables/sig_SOFCTvsOOFCT.csv", row.names = 1)
-sig_SOMCAvsOOMCA<-read.csv("https://raw.githubusercontent.com/SashaBurgess/Sasha-Honors-Thesis/main/Final-tables/sig_SOMCAvsOOMCA.csv", row.names = 1)
-sig_SOFCAvsOOFCA<-read.csv("https://raw.githubusercontent.com/SashaBurgess/Sasha-Honors-Thesis/main/Final-tables/sig_SOFCAvsOOFCA.csv", row.names = 1)
 
-SOMCHvsOOMCH<-read.csv("https://raw.githubusercontent.com/SashaBurgess/Sasha-Honors-Thesis/main/Final-tables/SOMCHvsOOMCH.csv", row.names = 1)
-SOFCHvsOOFCH<-read.csv("https://raw.githubusercontent.com/SashaBurgess/Sasha-Honors-Thesis/main/Final-tables/SOFCHvsOOFCH.csv", row.names = 1)
-SOMCTvsOOMCT<-read.csv("https://raw.githubusercontent.com/SashaBurgess/Sasha-Honors-Thesis/main/Final-tables/SOMCTvsOOMCT.csv", row.names = 1)
-SOFCTvsOOFCT<-read.csv("https://raw.githubusercontent.com/SashaBurgess/Sasha-Honors-Thesis/main/Final-tables/SOFCTvsOOFCT.csv", row.names = 1)
-SOMCAvsOOMCA<-read.csv("https://raw.githubusercontent.com/SashaBurgess/Sasha-Honors-Thesis/main/Final-tables/SOMCAvsOOMCA.csv", row.names = 1)
-SOFCAvsOOFCA<-read.csv("https://raw.githubusercontent.com/SashaBurgess/Sasha-Honors-Thesis/main/Final-tables/SOFCAvsOOFCA.csv", row.names = 1)
+
+
+deanalysis=sig_SOMCTvsOOMCT
+Title= "Volcano Plot SOMCTvsOOMCT comparison"
+
+sigde=deanalysis[deanalysis[,"FDR"]<=.05,]
+userLog2fc = 2.0
+userFDR = 0.05
+colorStyle="RedBlue"
+axisLab = 25
+
+ENSEMBLList <- as.list(org.Dm.eg.db)
+deanalysis$geneid <- ENSEMBLList[row.names(deanalysis)]
+
+
+deanalysis$genesymbol <- lookUp(as.character(deanalysis$geneid), 'org.Mm.eg', 'SYMBOL')
+deanalysis=as.matrix(deanalysis)
+
+plot <- EnhancedVolcano(deanalysis,
+                        lab = deanalysis$genesymbol,
+                        x = "logFC",
+                        y = "FDR",
+                        xlim = c(-6, 10),ylim = c(0, 6),
+                        title = Title,
+                        caption = paste('FC cutoff = ',sprintf("%.1f", round(userLog2fc,1)),', FDR cutoff = ',userFDR),
+                        xlab = bquote(~Log[2]~ "fold change"),
+                        ylab = bquote(~-Log[10]~adjusted~italic(P)/FDR),
+                        axisLabSize = axisLab, #default = 18
+                        pCutoff = userFDR,
+                        FCcutoff = userLog2fc,
+                        labSize = 3,
+                        colAlpha = 1,
+                        legend=c('NS','Log2 FC','Adjusted p-value (FDR)',
+                                 'Adjusted p-value & Log2 FC'),
+                        legendPosition = 'top',
+                        legendLabSize = 10,
+                        legendIconSize = 3.0)
+
 
